cd8be64325e9673626703007c7c34157d9a9b6aa
Vijay khot <120132133+khvijay@users.noreply.github.com>
2024-02-23 00:21:06 +0530

    Add files via upload

---
+++ While loop.py
@@ -0,0 +1,13 @@
+i = int(input("enter the number: "))
+while(i<=38):
+    i = int(input("enter the number: "))
+    print(i)
+    
+    print("done with the loop")
+    
+ count =5
+while(count>0):
+    print(count)
+    count = count - 1
+else:
+    print("im inside the else") 
